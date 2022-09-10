a0600a925d04e5fcf1045be67db46d55b7be6811
ting-prado <ethel.prado1998@gmail.com>
2022-09-10 13:04:10 +0800

    added each card information

---
+++ src/cardsInfo.js
@@ -0,0 +1,85 @@
+import dipper from './images/dipper.webp';
+import mabel from './images/mabel.webp';
+import stan from './images/grunkle_stan.webp';
+import soos from './images/soos.webp';
+import wendy from './images/wendy.webp';
+import ford from './images/ford.webp';
+import bill from './images/bill.webp';
+import waddles from './images/waddles.webp';
+import gompers from './images/gompers.webp';
+import mcgucket from './images/mcgucket.webp';
+import gideon from './images/gideon.webp';
+import susan from './images/susan.webp';
+import blubs from './images/blubs.webp';
+import durland from './images/durland.webp';
+import pacifica from './images/pacifica.webp';
+import robbie from './images/robbie.webp';
+
+const cardsInfo = [
+  {
+    name: 'Dipper Pines',
+    image: dipper,
+  },
+  {
+    name: 'Mabel Pines',
+    image: mabel,
+  },
+  {
+    name: 'Grunkle Stan',
+    image: stan,
+  },
+  {
+    name: 'Soos Ramirez',
+    image: soos,
+  },
+  {
+    name: 'Wendy Corduroy',
+    image: wendy,
+  },
+  {
+    name: 'Pacifica Northwest',
+    image: pacifica,
+  },
+  {
+    name: 'Robbie Valentino',
+    image: robbie,
+  },
+  {
+    name: 'Ford Pines',
+    image: ford,
+  },
+  {
+    name: 'Bill Cipher',
+    image: bill,
+  },
+  {
+    name: 'Waddles',
+    image: waddles,
+  },
+  {
+    name: 'Gompers the Goat',
+    image: gompers,
+  },
+  {
+    name: 'Old Man McGucket',
+    image: mcgucket,
+  },
+  {
+    name: 'Gideon Gleeful',
+    image: gideon,
+  },
+  {
+    name: 'Lazy Susan',
+    image: susan,
+  },
+  {
+    name: 'Sheriff Blubs',
+    image: blubs,
+  },
+  {
+    name: 'Deputy Durland',
+    image: durland,
+  },
+];
+
+export default cardsInfo;
