f4a9adbde6807fea58b361ef0e2a65930393355f
ting-prado <ethel.prado1998@gmail.com>
2022-09-11 13:17:14 +0800

    finalized component structure

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
-import { useState } from 'react';
 import '../styles/Card.css';
 
 const Card = (props) => {
-  const { name, imgSrc } = props;
+  const { id, name, imgSrc, handleClick } = props;
   return (
     <div className="Card">
+      <div onClick={handleClick} className="cover" id={id}></div>
       <img src={imgSrc} alt={name.toLowerCase()} />
       <p>{name}</p>
     </div>
